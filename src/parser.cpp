ed3817082fff11e5ecebf05808135c997bba98de
upprsk <lucassilveiraross@gmail.com>
2025-03-14 10:10:59 -0300

    feat: :sparkles: added ASSERT() for parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,6 +7,7 @@
 
 #include "ast.hpp"
 #include "error_reporter.hpp"
+#include "libassert/assert.hpp"
 #include "span.hpp"
 #include "tokenizer.hpp"
 
@@ -18,6 +19,7 @@ struct Parser {
     }
 
     [[nodiscard]] constexpr auto peek_prev() const -> Token {
+        ASSERT(!tokens.empty());
         return tokens[current - 1];
     }
 
